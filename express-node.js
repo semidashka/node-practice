9eff424438f3388b4814e058bd43d1bf6590fbd1
Daria Kuzmina <daria.n.kuzmina@gmail.com>
2022-01-28 18:47:22 +0200

    feat: /list-of-usernames

--- express-node.js
+++ express-node.js
@@ -7,6 +7,13 @@ const app = express();
 
 app.use(express.urlencoded({extended: false}));
 
+const filePath = path.join(__dirname, 'data', 'users.json');
+
+function readJson(somePath) {
+  const fileData = fs.readFileSync(somePath);
+  return JSON.parse(fileData);
+}
+
 app.get("/currenttime", function (req, res) {
   res.send("<h1>" + new Date().toISOString() + "</h1>");
 });
@@ -18,10 +25,7 @@ app.get("/", function (req, res) {
 app.post('/store-user', function(req, res) {
   
   const userName = req.body.username;
-
-  const filePath = path.join(__dirname, 'data', 'users.json')
-  const fileData = fs.readFileSync(filePath);
-  const existingUsers = JSON.parse(fileData);
+  const existingUsers = readJson(filePath);
 
   existingUsers.push(userName);
 
@@ -29,5 +33,23 @@ app.post('/store-user', function(req, res) {
 
   res.send('<h1>Username stored!</h1>')
 })
+
+app.get('/list-of-usernames', function (req, res) {
+
+  const usersArr = readJson(filePath);
+
+  let resDataStr = '<ul>';
+  
+  for (const user of usersArr) {
+    resDataStr += `<li>${user}</li>`;
+  }
+
+  resDataStr += '</ul>';
+
+  res.send(resDataStr);
+
+}) 
+
+
 
 app.listen(3000);
