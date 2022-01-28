12193762cf47cce33c66ac20d30004f511a56d0c
Daria Kuzmina <daria.n.kuzmina@gmail.com>
2022-01-28 19:54:53 +0200

    handle  requests

--- pure-node.js
+++ pure-node.js
@@ -1,12 +1,12 @@
 const http = require('http');
 
-function handleRequest(request, response) {
-  if (request.url === '/currenttime') {
-    response.statusCode = 200;
-    response.end('<h1>' + new Date().toISOString() + '</h1>');
+function handleRequest(req, res) {
+  if (req.url === '/currenttime') {
+    res.statusCode = 200;
+    res.end('<h1>' + new Date().toISOString() + '</h1>');
   } else if (request.url === '/') {
-    response.statusCode = 200;
-    response.end('<h1>Hello World!</h1>');
+    res.statusCode = 200;
+    res.end('<h1>Hello World!</h1>');
   }
 }
 
